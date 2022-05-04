9cba2a618019cdc3158d322c9953690665bbfbbc
suraksha <durga232@gmail.com>
2022-05-04 12:05:00 +0530

    multilambda

--- getLambda/LearnMultiLambdaMeta.py
+++ getLambda/LearnMultiLambdaMeta.py
@@ -75,103 +75,68 @@ class LearnMultiLambdaMeta(object):
             Python dictionary object containing models parameters
         """
         self.model.load_state_dict(model_params)
+        self.model.eval()
 
     
     def get_lambdas(self, eta,epoch,lam):
 
-        offset = 0
-        batch_wise_indices = list(self.trainloader.batch_sampler)
-        #eta =0.1
-        
-        #self.smoothLoss.smoothing = 0.4 
-        lambdas = lam.cuda(0)#, device=self.device)
-        #soft_lam = F.softmax(lambdas, dim=1)
-        smoothing = 0.2
-        '''with torch.no_grad():
+        self.model.eval()
+        for m in range(len(self.teacher_model)):
+            self.teacher_model[m].eval()
+        with torch.no_grad():
+            val_loss = 0
+            val_total = 0
             for batch_idx, (inputs, targets) in enumerate(self.valloader):
-                inputs, targets = inputs.to(self.device), targets.to(self.device, non_blocking=True)
-                if batch_idx == 0:
-                    out, l1 = self.model(inputs, last=True, freeze=True)
-                    self.init_out = out
-                    self.init_l1 = l1
-                    #self.y_val = targets  # .view(-1, 1)
-                    tea_out_val = self.teacher_model[-1](inputs)
-                else:
-                    out, l1 = self.model(inputs, last=True, freeze=True)
-                    self.init_out = torch.cat((self.init_out, out), dim=0)
-                    self.init_l1 = torch.cat((self.init_l1, l1), dim=0)
-                    #self.y_val = torch.cat((self.y_val, targets), dim=0)
-                    tea_out_val = torch.cat((tea_out_val, self.teacher_model[-1](inputs)), dim=0)'''
+                # print(batch_idx)
+                inputs, targets = inputs.to(self.device), targets.to(self.device)
+                outputs = self.model(inputs)
+                loss = self.criterion_red(outputs, targets)
+                val_loss += targets.size(0)*loss.item()
+                val_total += targets.size(0)
+                print(val_loss/val_total,end=",")
+        print()
 
-        # val_loss_SL = self.criterion_red(self.init_out,self.y_val)
-
-        #val_loss_SL = torch.sum(self.criterion(self.init_out, self.y_val))
-
-        # print(val_loss_SL)
-
-        '''val_loss_KD = nn.KLDivLoss(reduction='none')(F.log_softmax(self.init_out / self.temp, dim=1),\
-            F.softmax(tea_out_val / self.temp, dim=1))
-        val_loss_KD = self.temp*self.temp*torch.sum (val_loss_KD)#torch.mean(torch.sum (val_loss_KD,dim=1))'''
-
-        # val_loss_KD = self.temp*self.temp*nn.KLDivLoss(reduction='batchmean')(F.log_softmax(self.init_out / self.temp, dim=1),\
-        #    F.softmax(tea_out_val / self.temp, dim=1))
-
-        #self.init_out = self.init_out.cuda(0)
-        #self.init_l1 = self.init_l1.cuda(0)
-        #self.y_val = self.y_val.cuda(0)
-        #tea_out_val = tea_out_val.cuda(0)
-
+        smoothing = 0.2
+        
         KD_grads = [0 for _ in range(len(self.teacher_model))]
         '''if epoch > 100:
             c_temp = 1 #self.temp
         else:
             c_temp = self.temp'''
-        c_temp = 1
+        c_temp = 1#self.temp
+        max_value = 2
         dataloader_iterator = iter(self.valloader)
         for batch_idx, (inputs, target,indices) in enumerate(self.trainloader):
 
             #batch_wise_indices = list(self.trainloader.batch_sampler)
-
-            with torch.no_grad():
-
-                try:
-                    X, Y = next(self.valloader)
-                except:
-                    dataloader_iterator = iter(self.valloader)
-                    X, Y = next(dataloader_iterator)
-
-                out, l1 = self.model(X.to(self.device), last=True, freeze=True)
-                self.init_out = out
-                self.init_l1 = l1
-                tea_out_val = self.teacher_model[-1](X.to(self.device))
-
-
+            self.model.zero_grad()
             inputs, target = inputs.to(self.device), target.to(self.device, non_blocking=True)
 
             outputs, l1 = self.model(inputs, last=True, freeze=True)
-            
+            #print(l1.shape)
             loss_SL = self.criterion_red(outputs, target)  # self.criterion(outputs, target).sum()
 
-            l0_grads = (torch.autograd.grad(loss_SL, outputs)[0]).detach().clone().cuda(0)
+            l0_grads = (torch.autograd.grad(loss_SL, outputs)[0]).detach().clone().to(self.device)
             l0_expand = torch.repeat_interleave(l0_grads, l1.shape[1], dim=1)
-            l1_grads = l0_expand * l1.repeat(1, self.num_classes).cuda(0)
+            l1_grads = l0_expand * l1.detach().repeat(1, self.num_classes).to(self.device)
 
             if batch_idx % self.fit == 0:
                 with torch.no_grad():
-                    train_out = outputs.cuda(0)
-                    train_l1 = l1.cuda(0)
-                    train_target = target.cuda(0)
-                    tea_out = self.teacher_model[-1](inputs).cuda(0)
+                    train_out = outputs.detach().to(self.device)
+                    train_l1 = l1.detach().to(self.device)
+                    train_target = target.to(self.device)
+                    tea_out = self.teacher_model[-1](inputs).to(self.device)
                 SL_grads = torch.cat((l0_grads, l1_grads), dim=1)
                 batch_ind = list(indices) #batch_wise_indices[batch_idx]
             else:
                 with torch.no_grad():
-                    train_out = torch.cat((train_out,outputs.cuda(0)), dim=0)
-                    train_l1 = torch.cat((train_l1,l1.cuda(0)), dim=0)
-                    train_target = torch.cat((train_target,target.cuda(0)), dim=0)
-                    tea_out = torch.cat((tea_out, self.teacher_model[-1](inputs).cuda(0)), dim=0)
+                    train_out = torch.cat((train_out,outputs.detach().to(self.device)), dim=0)
+                    train_l1 = torch.cat((train_l1,l1.detach().to(self.device)), dim=0)
+                    train_target = torch.cat((train_target,target.to(self.device)), dim=0)
+                    tea_out = torch.cat((tea_out, self.teacher_model[-1](inputs).to(self.device)), dim=0)
                 SL_grads = torch.cat((SL_grads, torch.cat((l0_grads, l1_grads), dim=1)), dim=0)
                 batch_ind.extend(list(indices))#batch_wise_indices[batch_idx])
+            #print(SL_grads.shape)
 
             for m in range(len(self.teacher_model)):
                 with torch.no_grad():
@@ -180,35 +145,59 @@ class LearnMultiLambdaMeta(object):
                     F.log_softmax(outputs / self.temp, dim=1), \
                     F.softmax(teacher_outputs / self.temp, dim=1))
 
-                #loss_KD = nn.KLDivLoss(reduction='batchmean')(F.log_softmax(outputs, dim=1), \
-                #    F.softmax(teacher_outputs , dim=1))
-
-                #loss_KD = torch.mean(torch.sum(-F.softmax(teacher_outputs, dim=1) * \
-                #F.log_softmax(outputs, dim=1),dim=1))
-
-                l0_grads = (torch.autograd.grad(loss_KD, outputs)[0]).detach().clone().cuda(0)
+                l0_grads = (torch.autograd.grad(loss_KD, outputs)[0]).detach().clone().to(self.device)
                 l0_expand = torch.repeat_interleave(l0_grads, l1.shape[1], dim=1)
-                l1_grads = l0_expand * l1.repeat(1, self.num_classes).cuda(0)
+                l1_grads = l0_expand * l1.detach().repeat(1, self.num_classes).to(self.device)
 
                 if batch_idx % self.fit == 0:
                     KD_grads[m] = torch.cat((l0_grads, l1_grads), dim=1)
                 else:
                     KD_grads[m] = torch.cat((KD_grads[m], torch.cat((l0_grads, l1_grads), dim=1)), dim=0)
 
+                #print(batch_idx , self.fit)
+                #print(KD_grads[0].shape)
             if (batch_idx + 1) % self.fit == 0 or batch_idx + 1 == len(self.trainloader):
+
+                with torch.no_grad():
+
+                    try:
+                        X, Y = next(dataloader_iterator)
+                    except:
+                        dataloader_iterator = iter(self.valloader)
+                        X, Y = next(dataloader_iterator)
+
+                    out, l1 = self.model(X.to(self.device), last=True, freeze=True)
+                    self.init_out = out.detach().to(self.device)
+                    self.init_l1 = l1.detach().to(self.device)
+                    for t in range(len(self.teacher_model)):
+                        temp = self.teacher_model[t](X.to(self.device)).to(self.device)
+                        if t == 0:
+                            t_max = self.criterion_red(temp, Y.to(self.device))
+                            tea_out_val = temp
+                        else:
+                            if t_max > self.criterion_red(temp, Y.to(self.device)):
+                                t_max = self.criterion_red(temp, Y.to(self.device))
+                                tea_out_val = temp
+
                 
-                #val_init = self.init_out.clone()
                 for r in range(5):
+
+                    lambdas = lam.clone().to(self.device)#, device=self.device)
+                    lambdas.requires_grad = True
                     #print("Before",lambdas[batch_ind[0]].item(),lambdas[batch_ind[-1]].item())
                     
                     #comb_grad = soft_lam[batch_ind,0][:,None]*SL_grads 
-                    comb_grad = lambdas[batch_ind,0][:,None]*SL_grads 
+                    lambdas1 = lambdas[batch_ind,0][:,None]
+                    comb_grad_all = lambdas1*SL_grads 
                     
+                    lambdas_2 =[]
                     for m in range(len(self.teacher_model)):
+                        lambdas_2.append(lambdas[batch_ind,m+1][:,None])
                         #comb_grad += soft_lam[batch_ind,m+1][:,None]*KD_grads[m]
-                        comb_grad += lambdas[batch_ind,m+1][:,None]*KD_grads[m]
+                        comb_grad_all += lambdas_2[m]*KD_grads[m]
 
-                    comb_grad = comb_grad.sum(0)
+
+                    comb_grad = comb_grad_all.sum(0)
 
                     out_vec_val = self.init_out - (eta * comb_grad[:self.num_classes].view(1, -1).\
                         expand(self.init_out.shape[0], -1))
@@ -216,102 +205,33 @@ class LearnMultiLambdaMeta(object):
                     out_vec_val = out_vec_val - (eta * torch.matmul(self.init_l1, comb_grad[self.num_classes:].\
                         view(self.num_classes, -1).transpose(0, 1)))
 
-                    #val_init = out_vec_val.clone()
-                    out_vec_val.requires_grad = True
-                    #loss_SL_val = self.criterion_red(out_vec_val,Y.to(self.device) )#self.y_val)
-                    #self.smoothLoss(out_vec_val, self.y_val)  
+                    #out_vec_val.requires_grad = True
+                    loss_SL_val = self.criterion_red(out_vec_val, Y.to(self.device))
 
-                    true_y = torch.zeros_like(tea_out_val)
-                    true_y.scatter_(1, Y.to(self.device).data.unsqueeze(1), smoothing)
-                    soft_teacher = ((1-smoothing)*F.softmax(tea_out_val/c_temp, dim=1) + true_y)
-                    loss_KD_val = c_temp * c_temp *nn.KLDivLoss(reduction='batchmean')(F.log_softmax(\
-                    out_vec_val/c_temp , dim=1), soft_teacher)
+                    alpha_grads =  (torch.autograd.grad(loss_SL_val, lambdas1,retain_graph=True)[0]).detach().clone().to(self.device)  
+                    lam[batch_ind,0] = lam[batch_ind,0] - 40*alpha_grads.view(-1)
 
-                    #l0_grads = (torch.autograd.grad(0.25*loss_KD_val+0.75*loss_SL_val, out_vec_val)[0]).detach().\
-                    #clone().cuda(0)
-                    l0_grads = (torch.autograd.grad(loss_KD_val, out_vec_val)[0]).detach().clone().cuda(0)
-                    #print(round(loss_KD_val.item(),4), end=",")
-
-                    #print(round(loss_SL_val.item(),4), end=",")
-                    l0_expand = torch.repeat_interleave(l0_grads, self.init_l1.shape[1], dim=1)
-                    l1_grads = l0_expand * self.init_l1.repeat(1, self.num_classes).cuda(0)
-                    up_grads_val = torch.cat((l0_grads, l1_grads), dim=1).sum(0)
-
-                    out_vec = train_out.detach().clone() - (eta * comb_grad[:self.num_classes].view(1, -1)\
-                    .expand(train_out.shape[0], -1))
-
-                    out_vec = out_vec - (eta * torch.matmul(train_l1, comb_grad[self.num_classes:].\
-                        view(self.num_classes, -1).transpose(0, 1)))
-
-                    #train_out = out_vec.clone()
-                    out_vec.requires_grad = True
-
-                    #loss_SL = self.criterion_red(out_vec, train_target) #self.smoothLoss(out_vec, train_target)   
-
-                    true_y = torch.zeros_like(tea_out)
-                    true_y.scatter_(1, train_target.to(self.device).data.unsqueeze(1), smoothing)
-                    soft_teacher = ((1-smoothing)*F.softmax(tea_out/c_temp, dim=1) + true_y)
-                    #soft_teacher = F.softmax(tea_out/c_temp, dim=1) 
-                    loss_KD = c_temp * c_temp *nn.KLDivLoss(reduction='batchmean')(F.log_softmax(\
-                    out_vec/c_temp , dim=1), soft_teacher)
-
-                    if (batch_idx + 1) % (self.fit*40) ==0:
-                        #print(round(loss_SL_val.item(),4),"+",round(loss_KD_val.item(),4),"+",\
-                        #round(loss_SL.item(),4),"+",round(loss_KD.item(),4), end=",")
-                        print(round(loss_KD_val.item(),4),"+",round(loss_KD.item(),4), end=",")
-
-                    #l0_grads = (torch.autograd.grad(0.25*loss_KD+0.75*loss_SL, out_vec)[0]).detach().clone().cuda(0)
-                    l0_grads = (torch.autograd.grad(loss_KD, out_vec)[0]).detach().clone().cuda(0)
-                    l0_expand = torch.repeat_interleave(l0_grads, train_l1.shape[1], dim=1)
-                    l1_grads = l0_expand * train_l1.repeat(1, self.num_classes).cuda(0)
-                    up_grads = torch.cat((l0_grads, l1_grads), dim=1).sum(0)
-
-                    
-                    combined = (0.9*up_grads_val+0.1*up_grads).T
-
-
-                    """tea_val = torch.max(F.log_softmax(tea_out_val/c_temp, dim=1),dim=1).values
-                    stu_val = F.log_softmax(out_vec_val/c_temp, dim=1)[torch.arange(out_vec_val.shape[0]),self.y_val]
-                    loss_val = self.squareLoss(tea_val,stu_val)
-
-                    if (batch_idx + 1) % (self.fit*40) ==0:
-                        print(round(loss_val.item(),4), end=",")
-
-                    l0_grads = (torch.autograd.grad(loss_val, out_vec_val)[0]).detach().clone().cuda(0)
-                    l0_expand = torch.repeat_interleave(l0_grads, self.init_l1.shape[1], dim=1)
-                    l1_grads = l0_expand * self.init_l1.repeat(1, self.num_classes).cuda(0)
-                    combined = torch.cat((l0_grads, l1_grads), dim=1).sum(0)"""
-
-                    '''grad = ((1-soft_lam[batch_ind,0])*soft_lam[batch_ind,0])[:,None]*SL_grads
-                    #grad = SL_grads 
-                    for m_1 in range(len(self.teacher_model)):
-                        grad -= (soft_lam[batch_ind,0]*soft_lam[batch_ind,m_1+1])[:,None]*KD_grads[m_1]
-                        #grad -= KD_grads[m_1]
-                    alpha_grads = torch.matmul(grad,combined)
-                    lambdas[batch_ind,0] = lambdas[batch_ind,0] +  6000*eta*alpha_grads #9*eta*'''
-                    
                     for m in range(len(self.teacher_model)):
-                        #grad = (-soft_lam[batch_ind,0]*soft_lam[batch_ind,m+1])[:,None]*SL_grads 
-                        grad = KD_grads[m] -SL_grads 
-                        '''for m_1 in range(len(self.teacher_model)):
-                            if m_1 == m:
-                                #grad += ((1-soft_lam[batch_ind,m_1+1])*soft_lam[batch_ind,m_1+1])[:,None]*KD_grads[m_1]
-                                grad += KD_grads[m_1]
-                            else:
-                                #grad -= (soft_lam[batch_ind,m+1]*soft_lam[batch_ind,m_1+1])[:,None]*KD_grads[m_1]
-                                grad -= KD_grads[m_1]'''
-                        alpha_grads = torch.matmul(grad,combined)
-                        lambdas[batch_ind,m+1] = lambdas[batch_ind,m+1] +  100*eta*alpha_grads #9*eta*
+                        
+                        alpha_grads =  (torch.autograd.grad(loss_SL_val, lambdas_2[m])[0]).detach().clone().to(self.device)  
+                        lam[batch_ind,m+1] = lam[batch_ind,m+1] - 40*alpha_grads.view(-1) #9*eta*"""
+                        #lam[batch_ind,0] = lam[batch_ind,0] + 100*alpha_grads.view(-1)
+
+                    if (batch_idx + 1) % (self.fit*40) ==0:
+                        
+                        if r ==0:
+                            print(round(self.criterion_red(self.init_out, Y.to(self.device)).item(),4))
+                        print(alpha_grads[0],round(loss_SL_val.item(),4),end=",")
+                        
+                    del out_vec_val
                     
-                    #print("After",lambdas[batch_ind[0]].item(),lambdas[batch_ind[-1]].item())
-                    lambdas.clamp_(min=1e-7,max=1-1e-7)
-                    #soft_lam = F.softmax(lambdas, dim=1)
-                    lambdas[batch_ind,0] = 1- torch.max(lambdas[batch_ind,1:],dim=1).values
+                    lam.clamp_(min=1e-7,max=max_value-1e-7)
+                    
                 if (batch_idx + 1) % (self.fit*40) ==0:
-                    print()#"End for loop")
+                    print()
 
         #lambdas.clamp_(min=0.01,max=0.99)
-        return lambdas.cuda(0)
+        return lam
 
 
 
